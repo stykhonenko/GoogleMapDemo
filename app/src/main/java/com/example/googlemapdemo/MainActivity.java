4b8bfd8c58217e8246c02adf673a0e1a7aea393a
Sergiy Tykhonenko <stykhonenko@css.edu>
2019-05-06 22:53:55 -0500

    Initial commit

---
+++ app/src/main/java/com/example/googlemapdemo/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.googlemapdemo;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng Tatarska = new LatLng(50.467712, 30.486326);
+        map.addMarker(new MarkerOptions().position(Tatarska).title("Tatarska"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(Tatarska));
+    }
+}
